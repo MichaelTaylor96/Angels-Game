2efc1c2ac5fce67a68cac006c66f92f78e393300
MichaelTaylor96 <michael96taylor@gmail.com>
2019-11-28 18:23:16 -0500

    Setup basic player controls

--- js-angels/main.js
+++ js-angels/main.js
@@ -1,14 +1,65 @@
-const M = 50;
+const GRAVITY = 2000;
+
+class Player {
+    constructor(sprite) {
+        this.sprite = sprite;
+        this.sprite.allowRotation = true;
+        this.body = sprite.body;
+        this.rotation = 0;
+        this.grounded = false;
+        this.walled = false;
+        this.whichWall = false;
+        this.flip = false;
+
+        this.JUMP = -1000;
+        this.SPEED = 500;
+        this.TURN = 10;
+    }
+
+    update(cursors) {
+        this.grounded = this.body.blocked.down;
+        this.walled = this.body.blocked.right || this.body.blocked.left;
+        if (this.grounded || this.walled) this.rotation = 0;
+        this.sprite.angle = this.rotation % 360;
+        this.sprite.flipX = this.flip;
+
+        if (cursors.left.isDown) {
+            if (this.grounded) this.flip = true;
+            this.grounded ? this.sprite.setVelocityX(-this.SPEED) : this.rotation-=this.TURN;
+        }
+        else if (cursors.right.isDown) {
+            if (this.grounded) this.flip = false;
+            this.grounded ? this.sprite.setVelocityX(this.SPEED) : this.rotation+=this.TURN;
+        }
+        else if (this.grounded) {
+            this.sprite.setVelocityX(0);
+        }
+        if (this.walled) {
+            if (!this.grounded) this.sprite.setVelocityX(this.body.blocked.right ? 1 : -1);
+            this.whichWall = this.body.blocked.left;
+            this.flip = this.body.blocked.right;
+            if (this.body.velocity.y > 150) this.sprite.setVelocityY(150);
+        }
+        if (cursors.up.isDown) {
+            if (this.grounded) this.sprite.setVelocityY(this.JUMP);
+            else if (this.walled) {
+                this.sprite.setVelocityX(this.whichWall ? this.SPEED : -this.SPEED);
+                this.sprite.setVelocityY(this.JUMP+200);
+            }
+            console.log('JUMP')
+        }
+    }
+}
 
 var config = {
     type: Phaser.AUTO,
     width: 800,
-    height: 600,
+    height: 700,
     backgroundColor: '#FFFFFF',
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 50*M }
+            gravity: { y: GRAVITY }
         }
     },
     scene: {
@@ -30,29 +81,13 @@ function preload ()
 
 function create ()
 {
-    player = this.physics.add.image(400, 100, 'guy').setScale(2);
+    player = new Player(this.physics.add.image(400, 100, 'guy').setScale(2));
     cursors = this.input.keyboard.createCursorKeys();
 
     player.body.collideWorldBounds = true;
-    this.physics.add.collider(player, platforms);
 }
 
 function update ()
 {
-    if (cursors.left.isDown) {
-        player.setVelocityX(-10*M);
-        console.log('LEFT');
-    }
-    else if (cursors.right.isDown) {
-        player.setVelocityX(10*M);
-        console.log('RIGHT');
-    }
-    else {
-        player.setVelocityX(0);
-    }
-
-    if (cursors.up.isDown && player.body.blocked.down) {
-        player.setVelocityY(-22*M);
-        console.log('JUMP')
-    }
+    player.update(cursors);
 }
