9ff584c26875ae5703cf8fa67464081a1d28d0b3
MichaelTaylor96 <michael96taylor@gmail.com>
2019-11-28 21:05:16 -0500

    Setup basic camera scrolling

--- js-angels/main.js
+++ js-angels/main.js
@@ -12,8 +12,10 @@ class Player {
         this.flip = false;
 
         this.JUMP = -1000;
-        this.SPEED = 500;
-        this.TURN = 10;
+        this.MAXSPEED = 500;
+        this.ACCELERATION = 10000;
+        this.MAXFALL = 1000
+        this.TURN = 20;
     }
 
     update(cursors) {
@@ -23,28 +25,39 @@ class Player {
         this.sprite.angle = this.rotation % 360;
         this.sprite.flipX = this.flip;
 
+        if (this.body.velocity.y > this.MAXFALL) {
+            this.body.setVelocityY(this.MAXFALL);
+        }
+        if (Math.abs(this.body.velocity.x) > this.MAXSPEED) {
+            this.body.setAccelerationX(0);
+        }
         if (cursors.left.isDown) {
             if (this.grounded) this.flip = true;
-            this.grounded ? this.sprite.setVelocityX(-this.SPEED) : this.rotation-=this.TURN;
+            if (this.grounded && Math.abs(this.body.velocity.x) < this.MAXSPEED) {
+                this.body.setAccelerationX(-this.ACCELERATION);
+            } else { this.rotation-=this.TURN };
         }
         else if (cursors.right.isDown) {
             if (this.grounded) this.flip = false;
-            this.grounded ? this.sprite.setVelocityX(this.SPEED) : this.rotation+=this.TURN;
+            if (this.grounded && Math.abs(this.body.velocity.x) < this.MAXSPEED) {
+                this.body.setAccelerationX(this.ACCELERATION);
+            } else { this.rotation+=this.TURN };
         }
         else if (this.grounded) {
-            this.sprite.setVelocityX(0);
+            this.body.setVelocityX(0);
         }
         if (this.walled) {
-            if (!this.grounded) this.sprite.setVelocityX(this.body.blocked.right ? 1 : -1);
+            if (!this.grounded) this.body.setVelocityX(this.body.blocked.right ? 1 : -1);
             this.whichWall = this.body.blocked.left;
             this.flip = this.body.blocked.right;
-            if (this.body.velocity.y > 150) this.sprite.setVelocityY(150);
+            this.body.setAccelerationY(-1000);
         }
         if (cursors.up.isDown) {
-            if (this.grounded) this.sprite.setVelocityY(this.JUMP);
+            this.body.setVelocityY(this.JUMP);
+            if (this.grounded) this.body.setVelocityY(this.JUMP);
             else if (this.walled) {
-                this.sprite.setVelocityX(this.whichWall ? this.SPEED : -this.SPEED);
-                this.sprite.setVelocityY(this.JUMP+200);
+                this.body.setVelocityX(this.whichWall ? this.SPEED : -this.SPEED);
+                this.body.setVelocityY(this.JUMP+200);
             }
             console.log('JUMP')
         }
@@ -55,7 +68,6 @@ class Angel {
     constructor(sprite) {
         this.sprite = sprite;
         this.body = sprite.body;
-        this.sprite.setAccelerationY(-2000)
         this.body.mass = 0;
         this.TOPSPEED = -1500;
         this.ACCELERATION = (-1*(GRAVITY))-100;
@@ -63,9 +75,9 @@ class Angel {
 
     update() {
         if (this.body.velocity.y > this.TOPSPEED) {
-            this.sprite.setAccelerationY(this.ACCELERATION);
+            this.body.setAccelerationY(this.ACCELERATION);
         } else {
-            this.sprite.setVelocityY(this.TOPSPEED)
+            this.body.setVelocityY(this.TOPSPEED)
         };
 
         if (this.body.y < -200) this.destroy();
@@ -106,15 +118,22 @@ function preload ()
 {
     this.load.setBaseURL('img/');
     this.load.image('guy', 'AngelsGuy.png');
-    this.load.image('angel', 'angel.png')
+    this.load.image('angel', 'angel.png');
+    this.load.image('tiles', 'tiles.jpg')
 }
 
 function create ()
 {
-    player = new Player(this.physics.add.image(400, 780, 'guy').setScale(2));
+    this.physics.world.setBounds(0, 0, 800, 10000);
+    this.add.tileSprite(0, 0, 1600, 20000, 'tiles');
+    player = new Player(this.physics.add.sprite(400, 9980, 'guy').setScale(2));
     angels = [];
     cursors = this.input.keyboard.createCursorKeys();
     player.body.collideWorldBounds = true;
+
+    this.cameras.main.setViewport(0, 0, 800, 700);
+    this.cameras.main.setBounds(0, 0, 800, 10000);
+    this.cameras.main.startFollow(player.sprite)
 }
 
 function update ()
@@ -122,7 +141,7 @@ function update ()
     if (Math.floor(this.time.now/1000) !== seconds) {
         if (seconds % SPAWNRATE === 0) {
             var x = Math.random() * 750;
-            var angel = this.physics.add.image(x, 600, 'angel').setScale(.75);
+            var angel = this.physics.add.sprite(x, 9980, 'angel').setScale(.75);
             angels.push(new Angel(angel));
         }
         seconds = Math.floor(this.time.now/1000);
